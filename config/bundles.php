b26f6beba578bb27798e4161591b9489ff6c3940
Romain Ardiet <romain.ardiet@gmail.com>
2025-03-02 19:41:00 +0000

    Update dependencies (#37)
    * Update all Symfony components to 7.2
    
    * Revert doctrine-migrations-bundle to 3.3 in symfony.lock
    
    * Remove exact version constraint from doctrine-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Aws\Symfony\AwsBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
